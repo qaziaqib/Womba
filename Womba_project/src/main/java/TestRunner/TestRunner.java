97cd43c3b5d27ca6403ac624fff6889f7b20bd7f
Jyoti Singh <Jyoti Singh@DESKTOP-MT1LM8Q>
2020-10-05 16:50:50 +0530

    testng

--- Womba_project/src/main/java/TestRunner/TestRunner.java
+++ Womba_project/src/main/java/TestRunner/TestRunner.java
@@ -1,17 +1,25 @@
 package TestRunner;
 
+ 
+
+
 import org.junit.runner.RunWith;
 import org.testng.annotations.Test;
 
+ 
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.*;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/main/java/feature/WombapatientLogin.feature",
+@CucumberOptions(features="src/main/java/feature/WombapatientLogin.feature",tags="@test",
 glue= { "stepdefinition" })
 @Test
-public class TestRunner {
-	
-	//aba//
-	
+public class TestRunner extends AbstractTestNGCucumberTests{
+    
+    
+    
 
-	}
+ 
+
+    }
--- Womba_project/src/main/java/feature/WombapatientLogin.feature
+++ Womba_project/src/main/java/feature/WombapatientLogin.feature
@@ -1,3 +1,4 @@
+@test
 Feature: Login for patient
 Scenario: Login functionality with valid credentials
 
